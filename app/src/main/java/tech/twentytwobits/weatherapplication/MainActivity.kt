156330f0eade9cf8a01f1206a2fda0f8ec901763
Héctor Sabillón <h.hsabillon@gmail.com>
2019-10-22 10:19:42 -0600

    Initial commit

---
+++ app/src/main/java/tech/twentytwobits/weatherapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package tech.twentytwobits.weatherapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
