3420b3faca572a081c35ee053e14f00b26fbafec
Héctor Sabillón <h.hsabillon@gmail.com>
2019-10-24 10:50:28 -0600

    Solicitudes HTTP con Volley

--- app/src/main/java/tech/twentytwobits/weatherapplication/MainActivity.kt
+++ app/src/main/java/tech/twentytwobits/weatherapplication/MainActivity.kt
@@ -2,11 +2,47 @@ package tech.twentytwobits.weatherapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Verificar la conectividad a Internet
+        if (Network.verifyConnection(this)) {
+            Toast.makeText(this, "Conexión establecida", Toast.LENGTH_LONG).show()
+            httpVolley()
+        } else {
+            Toast.makeText(this, "No tienes conexión a Internet", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /***
+     * https://developer.android.com/training/volley/simple
+     * Realiza una petición http implementando Volley
+     */
+    private fun httpVolley() {
+        // Instanciar la cola de peticiones
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://www.google.com"
+
+        // Obtener un string de respuesta desde la url enviada
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> {
+                response -> Log.d("HTTPVolley", response)
+            },
+            Response.ErrorListener {
+                Log.d("HTTPVolley", "¡Ha ocurrido un error en la conexión!")
+            }
+        )
+
+        queue.add(stringRequest)
     }
 }
--- app/src/main/java/tech/twentytwobits/weatherapplication/Network.kt
+++ app/src/main/java/tech/twentytwobits/weatherapplication/Network.kt
@@ -1,2 +1,19 @@
 package tech.twentytwobits.weatherapplication
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.support.v7.app.AppCompatActivity
+
+/*
+    Verifica si existe conectividad de red y acceso a Internet
+ */
+class Network {
+    companion object {
+        fun verifyConnection(activity: AppCompatActivity): Boolean {
+            val connectivityManager = activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkInfo = connectivityManager.activeNetworkInfo
+
+            return networkInfo != null && networkInfo.isConnected
+        }
+    }
+}
